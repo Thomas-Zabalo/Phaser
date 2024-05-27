a5d5e6e1ddc796150a7614127187daba4b5366e1
Thomas ZABALO <thomas.zabalo@gmail.com>
2024-05-27 17:27:05 +0200

    Rework code button 3

--- src/script/main.js
+++ src/script/main.js
@@ -9,25 +9,26 @@ export default class Main extends Phaser.Scene {
 
     // Fonction de création des éléments du jeu
     create() {
-        const button1 = this.add.text(400, 200, 'Button 1', { backgroundColor: '#CCCCCC' })
+        const button1 = this.add.text(40, 200, 'Button 1', { backgroundColor: '#CCCCCC' })
             .setInteractive()
-            .on('pointerdown', function () {         
+            .on('pointerdown', function () {
                 localStorage.setItem('selectedButton', 'biker');
                 this.scene.start('game');
             }, this);
 
-        const button2 = this.add.text(400, 300, 'Button 2', { backgroundColor: '#CCCCCC' })
+        const button2 = this.add.text(200, 200, 'Button 2', { backgroundColor: '#CCCCCC' })
             .setInteractive()
             .on('pointerdown', function () {
                 localStorage.setItem('selectedButton', 'cyborg');
                 this.scene.start('game');
             }, this);
 
-        const button3 = this.add.text(400, 400, 'Button 3', { backgroundColor: '#CCCCCC' })
+        const button3 = this.add.text(360, 200, 'Button 3', { backgroundColor: '#CCCCCC' })
             .setInteractive()
             .on('pointerdown', function () {
                 localStorage.setItem('selectedButton', 'punk');
                 this.scene.start('game');
             }, this);
+
     }
 }
